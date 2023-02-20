c5f0eb843594ed6cfcfff229b1e3d67bc2cca8e4
seokiller3 <112966324+seokiller3@users.noreply.github.com>
2023-02-20 18:36:13 +0300

    Add files via upload

---
+++ Unit-tests-lesson-2-HW/src/Test/HW_Test/VehicleTest.java
@@ -0,0 +1,52 @@
+package seminar_2.HW;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+    @Test
+    public void testCarIsInstanceOfVehicle() {
+        Car car = new Car("Dodge", "Ram", 2010);
+        assertTrue(car instanceof Vehicle);
+    }
+    @Test
+    public void testCarHasFourWheels() {
+        Car car = new Car("Dodge", "Ram", 2010);
+        assertThat(car.getNumWheels()).isEqualTo(4);
+    }
+    @Test
+    public void testMotoHasTwoWheels() {
+        Motorcycle motorcycle = new Motorcycle("Kavasaki", "zx10100", 1990);
+        assertThat(motorcycle.getNumWheels()).isEqualTo(2);
+    }
+    @Test
+    public void testCarTestDriveSpeed() {
+        Car car = new Car("Dodge", "Ram", 2010);
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+    }
+    @Test
+    public void testMotoDriveSpeed() {
+        Motorcycle motorcycle = new Motorcycle("Kavasaki", "zx10100", 1990);
+        motorcycle.testDrive();
+        assertThat(motorcycle.getSpeed()).isEqualTo(75);
+    }
+    @Test
+    public void testCarTestDriveSpeedThanParkSpeed() {
+        Car car = new Car("Dodge", "Ram", 2010);
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+        car.park();
+        assertThat(car.getSpeed()).isEqualTo(0);
+    }
+    @Test
+    public void testMotoDriveSpeedThanParkSpeed() {
+        Motorcycle motorcycle = new Motorcycle("Kavasaki", "zx10100", 1990);
+        motorcycle.testDrive();
+        assertThat(motorcycle.getSpeed()).isEqualTo(75);
+        motorcycle.park();
+        assertThat(motorcycle.getSpeed()).isEqualTo(0);
+    }
+}
